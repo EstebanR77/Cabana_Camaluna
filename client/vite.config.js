5e134d103b032225f0cd278fdbc1c22fb1b643f9
SaraJB07 <sarbarrera@uniboyaca.edu.co>
2026-05-31 17:17:07 -0500

    Arreglo ngrok

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+    allowedHosts: ['tribrachic-marguerita-consumptively.ngrok-free.dev'],
     proxy: {
       '/api': 'http://localhost:3000',
       '/ws': {
