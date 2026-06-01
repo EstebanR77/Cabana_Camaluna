356ec70d6b8d239e4085814c5392110c13da6c83
SaraJB07 <sarbarrera@uniboyaca.edu.co>
2026-05-31 19:40:21 -0500

    Ngrok sara

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    allowedHosts: 'all',
+    allowedHosts: ['tribrachic-marguerita-consumptively.ngrok-free.dev'],
     proxy: {
       '/api': 'http://localhost:3000',
       '/ws': {
